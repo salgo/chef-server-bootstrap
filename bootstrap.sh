720b513dc438b69576d70406aa854e780041fda2
Andy Gale <andy@salgo.net>
2012-07-21 12:42:34 +0100

    First effort. Currently only tested with Debian Squeeze.

---
+++ bootstrap.sh
@@ -0,0 +1,99 @@
+#!/bin/sh 
+
+export TERM=xterm
+
+COOKBOOKS_PATH="/root/cookbooks"
+
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldylw=${txtbld}$(tput setaf 3) #  yellow
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldpur=${txtbld}$(tput setaf 5) #  purple
+bldcyn=${txtbld}$(tput setaf 5) #  cyan 
+bldwht=${txtbld}$(tput setaf 7) #  white
+txtrst=$(tput sgr0)             # Reset
+info=${bldwht}*${txtrst}        # Feedback
+pass=${bldblu}*${txtrst}
+warn=${bldred}*${txtrst}
+ques=${bldblu}?${txtrst}
+
+set -e
+
+echo
+echo "${txtbld}Chef Server bootstrap for Debian Squeeze${txtrst}"
+echo "- By Andy Gale <andy@salgo.net>"
+echo
+
+echo "${bldblu}Installing Ruby and dependencies${txtrst}"
+echo
+
+apt-get -y update 
+apt-get -y install ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl 
+
+echo
+echo "${bldred}Installing RubyGems${txtrst}"
+echo
+
+cd /tmp
+curl -O http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
+tar zxf rubygems-1.8.10.tgz
+cd rubygems-1.8.10
+sudo ruby setup.rb --no-format-executable
+
+echo
+echo "${bldgrn}Installing Chef${txtrst}"
+echo
+
+sudo gem install chef --no-ri --no-rdoc
+
+echo
+echo "${bldpur}Chef server setup${txtrst}"
+echo
+
+mkdir -p /etc/chef
+
+(cat << _EOF_
+file_cache_path "/tmp/chef-solo"
+cookbook_path "/root/cookbooks"
+_EOF_
+) > /etc/chef/solo.rb 
+
+(cat << _EOF_
+{
+  "chef_server": {
+    "server_url": "http://localhost:4000",
+    "webui_enabled": true,
+    "init_style": "runit"
+  },
+  "run_list": [ "recipe[chef-server::rubygems-install]" ]
+}
+_EOF_
+) > ~/chef.json
+
+echo "${bldblu}Fetching cookbook dependencies${txtrst}"
+
+mkdir -p $COOKBOOKS_PATH
+
+REQUIRED_COOKBOOKS="apache2 bluepill chef chef-server daemontools gecode nginx rabbitmq ucspi-tcp yum apt build-essential chef-client couchdb erlang java openssl runit xml zlib"
+
+for cookbook in $REQUIRED_COOKBOOKS; do
+    file="${COOKBOOKS_PATH}/${cookbook}"
+
+    if [[ ! -e $file ]]; then
+        wget http://github.com/opscode-cookbooks/$cookbook/tarball/master --output-document=${file}.tgz
+        mkdir -p $file
+        cd $file
+        tar --strip-components=1 -zxf ${file}.tgz
+        cd -
+    fi
+done
+
+chef-solo -c /etc/chef/solo.rb -j ~/chef.json 
+
+mkdir -p ~/.chef
+cp /etc/chef/validation.pem /etc/chef/webui.pem ~/.chef
+chown -R $USER ~/.chef
+
+echo "${bldcyn}Done.${txtrts}"
+
